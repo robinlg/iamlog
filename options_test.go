3a98d9d3a35f5fc1d2baaf7813de70edcf7da74b
Robin Liu <robinliu27@163.com>
2026-01-04 17:48:17 +0800

    feat: add options ut

---
+++ options_test.go
@@ -0,0 +1,24 @@
+package iamlog_test
+
+import (
+	"fmt"
+	"testing"
+
+	log "github.com/robinlg/iamlog"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Options_Validate(t *testing.T) {
+	opts := &log.Options{
+		Level:            "test",
+		Format:           "test",
+		EnableColor:      true,
+		EnableCaller:     false,
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	errs := opts.Validate()
+	expected := `[unrecognized level: "test" not a valid log format: "test"]`
+	assert.Equal(t, expected, fmt.Sprintf("%s", errs))
+}
